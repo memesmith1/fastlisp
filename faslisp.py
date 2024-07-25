42a06be9f673035095c26bfc0a5e82cb9038fac8
memesmith1 <115422357+memesmith1@users.noreply.github.com>
2024-07-24 22:33:17 -0700

    Update faslisp.py
    added fastlisp base functions

--- faslisp.py
+++ faslisp.py
@@ -1,1 +1,9 @@
-start file
+ibis = lambda x : x
+
+kestrel = lambda x : lambda y x
+
+kite = lambda x : lambda y : y
+
+viero = lambda x : lambda y : lambda z : z(x)(y)
+
+z = lambda f : (lambda x : f(lambda v : x(x)(v)))(lambda x : f(lambda v : x(x)(v)))
