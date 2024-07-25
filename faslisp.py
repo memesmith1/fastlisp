fe88cf89dfd1bc8be850ab3ff6c065e1f10c4faa
memesmith1 <115422357+memesmith1@users.noreply.github.com>
2024-07-24 22:35:16 -0700

    Update faslisp.py
    added some explanatory comments, may have used the wrong slash character

--- faslisp.py
+++ faslisp.py
@@ -1,9 +1,15 @@
+//if, quote
+
 ibis = lambda x : x
 
+//true, or, head
 kestrel = lambda x : lambda y x
 
+//false, and, tail
 kite = lambda x : lambda y : y
 
+//pair, cons, tuple
 viero = lambda x : lambda y : lambda z : z(x)(y)
 
+//recurse with an argument
 z = lambda f : (lambda x : f(lambda v : x(x)(v)))(lambda x : f(lambda v : x(x)(v)))
