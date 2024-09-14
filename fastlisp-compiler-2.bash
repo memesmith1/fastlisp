9e2484da652d2023ed73d4a5cdb9fcee75ca12d2
memesmith1 <115422357+memesmith1@users.noreply.github.com>
2024-09-14 15:36:32 -0700

    Create fastlisp-compiler-2.bash
    le compiler

---
+++ fastlisp-compiler-2.bash
@@ -0,0 +1,1 @@
+cat ~/fastlisp/fastlispsamplecode.fastlisp | python3 fl_1.py | python3 fl_2.py | python3 fl_3.py | python3 fl_4.py | python3 fl_5.py | python3 fl_6.py | python3 fl_7.py | python3 fl_8.py
