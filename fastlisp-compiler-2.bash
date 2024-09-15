dc824c3a0e15706aa28334a30dde8387a8d0dd85
memesmith1 <115422357+memesmith1@users.noreply.github.com>
2024-09-15 03:03:15 -0700

    Update fastlisp-compiler-2.bash
    added some code that would allow you to compile to blc instead of blc2 because I am testing it out on justine tunney's larger version of blc2

--- fastlisp-compiler-2.bash
+++ fastlisp-compiler-2.bash
@@ -1,1 +1,4 @@
-cat "$1" | python3 fl_1.py | python3 fl_2.py | python3 fl_3.py | python3 fl_4.py | python3 fl_5.py | python3 fl_6.py | python3 fl_7.py | python3 fl_8.py
+cat "$1" | python3 fl_1.py | python3 fl_2.py | python3 fl_3.py | python3 fl_4.py |
+    python3 fl_5_unary.py |
+    #python3 fl_5.py |  python3 fl_6.py | #uncomment this line and recomment the line above if you want blc2 which is more bit dense
+    python3 fl_7.py | python3 fl_8.py
